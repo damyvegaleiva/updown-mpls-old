d860a25cd42817f1144d5918fa87504a994424b5
DamyVegaLeiva <damyvegaleiva@gmail.com>
2022-08-15 23:50:38 -0500

    Back to the top button added.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,28 @@
-let counter = 1;
-setInterval(function () {
-  document.getElementById("radio" + counter).checked = true;
-  counter++;
-  if (counter > 6) {
-    counter = 1;
-  }
-}, 5000);
-
 let audio = new Audio();
 audio.src = "./audio/coin-sound.mp3";
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 2100 ||
+    document.documentElement.scrollTop > 2100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
